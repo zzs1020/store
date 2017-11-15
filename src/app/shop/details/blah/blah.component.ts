5d767ee20068e4e31f7e8f019c0c3c004b58be1d
Zeshi Zheng <mailtozeshi@gmail.com>
2017-11-14 19:33:09 -0500

    used ngb tabset

--- src/app/shop/details/blah/blah.component.ts
+++ src/app/shop/details/blah/blah.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'my-blah',
   templateUrl: './blah.component.html',
-  styleUrls: ['./blah.component.scss']
+  styleUrls: ['./blah.component.scss'],
+  providers: [NgbTabsetConfig]
 })
 export class BlahComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbTabsetConfig) {
+    config.justify = 'justified';
+  }
 
   ngOnInit() {
   }
