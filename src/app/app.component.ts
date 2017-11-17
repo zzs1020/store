2cf292b76cd772189090dc3d33283bf85e988bd4
Zeshi Zheng <mailtozeshi@gmail.com>
2017-11-16 21:07:13 -0500

    back button should show up except first time land on app or at main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { StatusService } from './shared/services/status/status.service';
 
@@ -10,21 +10,30 @@ import { StatusService } from './shared/services/status/status.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  historyCount: number = 0;
   showBack: boolean;
   menuOpen: boolean;
   headerColor: string;
   @HostBinding('style.overflow') overflow; // controlling body scrollability
   @HostBinding('style.position') position; // needed if want it works on ios
 
-  constructor(private router: Router, private status: StatusService) {
+  constructor(private router: Router, private status: StatusService) {}
+
+  ngOnInit() {
+    // except 'first time land in app' or 'at main page', all other should show back button
+    this.showBack = !!sessionStorage.getItem('showBack');
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
+        if (this.historyCount === 0) {
+          this.historyCount++;
+          sessionStorage.setItem('showBack', 'true');
+        }
+        this.showBack = !['/', '/store'].includes(event.url);
+
         if (event.url.includes('details')) {
-          this.showBack = true;
           this.headerColor = 'transparent';
         } else {
-          this.showBack = false;
           this.headerColor = '#06b1b9';
         }
       }
