66f80e20a9a7a4ef2212d37bda9ac6bf5e316814
Zeshi Zheng <mailtozeshi@gmail.com>
2017-11-16 20:14:28 -0500

    added pagination and sort

--- src/app/shop/all/all.component.ts
+++ src/app/shop/all/all.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDropdownConfig, NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'my-all',
   templateUrl: './all.component.html',
-  styleUrls: ['./all.component.scss']
+  styleUrls: ['./all.component.scss'],
+  providers: [NgbPaginationConfig, NgbDropdownConfig]
 })
 export class AllComponent implements OnInit {
+  currentPage: number;
+  menu = [
+    {value: 'low-high', desp: '价格:由低到高'},
+    {value: 'high-low', desp: '价格:由高到低'},
+    {value: 'popular', desp: '人气'},
+    {value: 'rating', desp: '评分'},
+    {value: 'new', desp: '上架时间'}];
+  selected: any = {};
 
-  constructor() { }
+  constructor(private config: NgbPaginationConfig) {
+    this.config.maxSize = 5; // todo: doesn't work because ngb bug
+    this.config.boundaryLinks = true;
+    this.config.rotate = true;
+  }
 
   ngOnInit() {
   }
 
+  onSelect(type) {
+    this.selected = type;
+  }
 }
