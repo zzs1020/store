600444d3249fb99fed9d56ccbec1e965f44124fb
Zeshi Zheng <mailtozeshi@gmail.com>
2017-11-18 16:47:35 -0500

    added some change to make travis tst working

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,11 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  // set up chrome for karma test
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -28,6 +33,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: { // special setup for travis karma test
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
